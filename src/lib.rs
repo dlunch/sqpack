10e96846b078c1a0921cfcea1eba4f9f03f585d6
Inseok Lee <dlunch@gmail.com>
2020-08-30 15:56:32 +0900

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+extern crate alloc;
+
+mod archive_id;
+mod error;
+mod package;
+mod raw_file;
+mod reference;
+mod util;
+
+pub use archive_id::SqPackArchiveId;
+pub use error::{Result, SqPackReaderError};
+pub use package::Package;
+pub use reference::{SqPackFileHash, SqPackFileReference};
+
+cfg_if::cfg_if! {
+    if #[cfg(feature = "std")] {
+        mod sqpack_reader;
+        pub use sqpack_reader::SqPackReader;
+    }
+}
---
+++ src/sqpack_reader/mod.rs
@@ -0,0 +1,53 @@
+mod archive;
+mod archive_container;
+mod data;
+mod definition;
+mod index;
+
+use alloc::boxed::Box;
+use std::io;
+use std::path::Path;
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use log::debug;
+
+use crate::archive_id::SqPackArchiveId;
+use crate::error::Result;
+use crate::package::Package;
+use crate::reference::SqPackFileReference;
+
+use archive::SqPackArchive;
+use archive_container::SqPackArchiveContainer;
+
+pub struct SqPackReader {
+    archives: SqPackArchiveContainer,
+}
+
+impl SqPackReader {
+    pub fn new(base_dir: &Path) -> io::Result<Self> {
+        Ok(Self {
+            archives: SqPackArchiveContainer::new(base_dir)?,
+        })
+    }
+
+    pub async fn archive(&self, archive_id: SqPackArchiveId) -> io::Result<Arc<SqPackArchive>> {
+        self.archives.get_archive(archive_id).await
+    }
+}
+
+#[async_trait]
+impl Package for SqPackReader {
+    async fn read_file_by_reference(&self, reference: &SqPackFileReference) -> Result<Vec<u8>> {
+        let archive = self.archive(reference.archive_id).await?;
+
+        let result = archive.read_file(reference.hash.folder, reference.hash.file).await;
+
+        #[cfg(debug_assertions)]
+        if result.is_err() {
+            debug!("No such file {}", reference.path);
+        }
+
+        result
+    }
+}
