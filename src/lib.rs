5aba0551c6452f6a2bab61a2a5b5be447d0995e3
Inseok Lee <dlunch@gmail.com>
2020-09-11 16:15:25 +0900

    pub mod definition

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ pub use package::Package;
 pub use reference::{SqPackFileHash, SqPackFileReference};
 
 pub mod internal {
-    mod definition {
+    pub mod definition {
         pub use crate::definition::*;
     }
     pub use crate::raw_file::SqPackRawFile;
