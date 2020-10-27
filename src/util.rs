ad8da8963130311e2b6ad9a6d22d2bd4fbec84db
Inseok Lee <dlunch@gmail.com>
2020-10-27 12:55:55 +0900

    Fix warning on no_std

--- src/util.rs
+++ src/util.rs
@@ -23,17 +23,17 @@ cfg_if::cfg_if! {
                 Ok(data)
             }
         }
+
+        pub fn cast_mut<T>(data: &mut [u8]) -> &mut T {
+            unsafe { &mut *(data.as_mut_ptr() as *mut T) }
+        }
+
+        pub fn cast_array<T>(data: &[u8]) -> &[T] {
+            unsafe { &*(data as *const [u8] as *const [T]) }
+        }
     }
 }
 
 pub fn cast<T>(data: &[u8]) -> &T {
     unsafe { &*(data.as_ptr() as *const T) }
-}
-
-pub fn cast_mut<T>(data: &mut [u8]) -> &mut T {
-    unsafe { &mut *(data.as_mut_ptr() as *mut T) }
-}
-
-pub fn cast_array<T>(data: &[u8]) -> &[T] {
-    unsafe { &*(data as *const [u8] as *const [T]) }
 }
